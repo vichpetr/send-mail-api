4c92eeaff89d3297ee4743ca89f09063d60ff08b
Petr Vích <vichpetr@gmail.com>
2021-04-19 08:24:32 +0200

    update cors

--- api/index.php
+++ api/index.php
@@ -1,5 +1,9 @@
 <?php
 
+header('Access-Control-Allow-Headers: *');
+header('Access-Control-Allow-Origin: *');
+header('Content-Type: application/json, charset=utf-8');
+
 use petrvich\sendmail\dto\InputRequest;
 use petrvich\sendmail\service\SendMailApiService;
 
@@ -7,6 +11,12 @@ require_once '../service/autoload.php';
 
 $json = file_get_contents('php://input');
 $data = json_decode($json, true);
+if(!is_array($data)) {
+    $responseData = new \petrvich\sendmail\dto\ResponseData(false);
+    $responseData->setMessage("wrong data input");
+    echo json_encode($responseData);
+    exit;
+}
 $request = new InputRequest($data);
 $apiService = new SendMailApiService($request);
 $response = $apiService->executeEmailSend();
